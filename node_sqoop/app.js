d49188a30696199704b79a1ecc21a1aefeccc364
zhkmxx9302013 <zhkmxx930@163.com>
2017-03-13 20:29:59 +0800

    data source ok

--- node_sqoop/app.js
+++ node_sqoop/app.js
@@ -5,8 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
 var users = require('./routes/users');
+var testconn = require('./routes/testconnection');
+var creatconn = require('./routes/createconnection');
 
 var app = express();
 
@@ -24,33 +27,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/snp/testconnection',testconn);
+app.use('/snp/createconnection',creatconn);
 
-app.all('*', function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-    res.header("X-Powered-By",' 3.2.1');
-    res.header("Content-Type", "application/json;charset=utf-8");
-    next();
-});
-
-//Post handler
-app.post('/snp/testconnection', function (req, res) {
-    var connUserName = req.body.connUserName;
-    var connPassword = req.body.connPassword;
-    var connUrl = req.body.connUrl;
-    console.log(connPassword + connUrl);
-    res.send("PASS");
-});
-
-
-app.post('/snp/createconnction', function (req, res) {
-    var connUserName = req.body.connUserName;
-    var connPassword = req.body.connPassword;
-    var connUrl = req.body.connUrl;
-
-    res.send("PASS");
-});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -70,7 +49,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(3097, function () {
     var host = server.address().address;
     var port = server.address().port;
 
--- snp_sqoop_web/assets/js/custom-zhkmxx/connection-page-control.js
+++ snp_sqoop_web/assets/js/custom-zhkmxx/connection-page-control.js
@@ -1,13 +1,36 @@
 /**
  * Created by zhao on 2017/3/12.
  */
+var existDatasource = "";
 
 $(document).ready(function () {
+    setDivHeight();
     testConnFunc();
     createConnFunc();
+    fetchExistDatasource();
+    setExistDatasourceView();
 });
 
+/**
+ * 获取已建立数据源
+ */
+function fetchExistDatasource(){
+    $.ajax({
+        url:createConnRouter,
+        dataType:"text",
+        type:"GET",
+        success: function(data){
+            existDatasource = data;
+        }
+    });
+}
 
+function setDivHeight(){
+    var height = $("#left-container").height();
+    console.log(height);
+    $("#right-container").css("max-height", height);
+    $("#right-container").css("overflow-y", "scroll");
+}
 function generateConnJson() {
     var connData = {
         "connName":$("#text_DSname").val(),
@@ -19,6 +42,7 @@ function generateConnJson() {
     }
     return connData;
 }
+
 
 function generateConnUrlFunc() {
     var data = generateConnJson();
@@ -61,23 +85,54 @@ function testFuncCallback(data, status){
 
 function createConnFunc() {
     $("#submitBtn").click(function () {
-        var data = generateConnJson();
+        fetchExistDatasource();
+        var dataJ = generateConnJson();
         var connPostUrl = generateConnUrlFunc();
         var postData = {
-            "connUserName":data[connUserName],
-            "connPassword":data[connPassword],
+            "connUserName":dataJ[connUserName],
+            "connPassword":dataJ[connPassword],
+            "connServer":dataJ[connServer],
+            "connPort":dataJ[connPort],
+            "connDBName":dataJ[connDBName],
+            "connName":dataJ[connName],
             "connUrl":connPostUrl}
-        $.ajax({
-            url:createConnRouter,
-            dataType:"text",
-            type:"POST",
-            data:postData,
-            success: function(data){
-                alert(data);
-            }
-        });
+        if(existDatasource.indexOf(dataJ[connName]) != -1){
+            alert("数据源名称已存在!");
+        }else{
+            $.ajax({
+                url:createConnRouter,
+                dataType:"text",
+                type:"POST",
+                data:postData,
+                success: function(data){
+                    if(data === "true"){
+                        addViewToDatasourceContainer(dataJ, postData);
+                    }
+                },
+                error: function(data){
+                    alert("ERROR==" + data);
+                }
+            });
+        }
+
         //$.post(submitConnRouter, postData, submitFuncCallback);
     });
+}
+
+function setExistDatasourceView(){
+    $.ajax({
+        url:createConnRouter,
+        dataType:"text",
+        type:"GET",
+        success: function(data){
+            var DSarray = data.split("??");
+            for(var i = 0; i < DSarray.length - 1;i++){
+                var itemArr = DSarray[i].split(",");
+                regenerateViewToDatasourceContainer(itemArr[0],itemArr[5],itemArr[3],itemArr[2],itemArr[1],itemArr[4]);
+            }
+        }
+    });
+
 }
 
 function createFuncCallback(data, status){
@@ -88,7 +143,47 @@ function createFuncCallback(data, status){
     }
 }
 
+function addViewToDatasourceContainer(data, postData){
+    var datasourceCardView = "<div class=\"col-md-12 col-sm-12\">" +
+    "<div class=\"card blue-grey darken-1\">"+
+        "<div class=\"card-content white-text\">"+
+            "<span class=\"card-title\">" +data[connName]+" <\/span>"+
+            "<p>数据源地址\:<label>" +data[connServer]+" <\/p>"+
+            "<p>数据源端口\:<label>" +data[connPort]+" <\/label><\/p>"+
+            "<p>数据库名称\:<label>" +data[connDBName]+" <\/label><\/p>"+
+            "<p>用  户  名\:<label>" +data[connUserName]+" <\/label><\/p>"+
+            "<p>连  接  串\:<label> >" +postData[connUrl]+" <\/label><\/p>"+
+        "<\/div>"+
+        "<div class=\"card-action\">"+
+            "<a href=\"#\">修改配置项<\/a>"+
+            "<a href=\"#\">测试连接项<\/a>"+
+            "<a href=\"#\">选择数据源<\/a>"+
+            "<a href=\"#\">删除数据源<\/a>"+
+        "<\/div>"+
+        "<\/div>";
+    $("#datasource-container").append(datasourceCardView);
+}
 
+function regenerateViewToDatasourceContainer(connName,connServer,connPort,connDBName,connUserName,connUrl){
+    var datasourceCardView = "<div class=\"col-md-12 col-sm-12\">" +
+        "<div class=\"card blue-grey darken-1\">"+
+        "<div class=\"card-content white-text\">"+
+        "<span class=\"card-title\">" +connName+" <\/span>"+
+        "<p>数据源地址\:<label>" +connServer+" <\/p>"+
+        "<p>数据源端口\:<label>" +connPort+" <\/label><\/p>"+
+        "<p>数据库名称\:<label>" +connDBName+" <\/label><\/p>"+
+        "<p>用  户  名\:<label>" +connUserName+" <\/label><\/p>"+
+        "<p>连  接  串\:<label> >" +connUrl+" <\/label><\/p>"+
+        "<\/div>"+
+        "<div class=\"card-action\">"+
+        "<a href=\"#\">修改配置项<\/a>"+
+        "<a href=\"#\">测试连接项<\/a>"+
+        "<a href=\"#\">选择数据源<\/a>"+
+        "<a href=\"#\">删除数据源<\/a>"+
+        "<\/div>"+
+        "<\/div>";
+    $("#datasource-container").append(datasourceCardView);
+}
 
 //Const Area
 var connName = "connName";
@@ -99,6 +194,7 @@ var connUserName = "connUserName";
 var connPassword = "connPassword";
 var connUrl = "connUrl";
 
-var url_prefix = "http://localhost:3000";
+var ip = "localhost";//"10.2.32.10";
+var url_prefix = "http://" + ip + ":3097";
 var testConnRouter = url_prefix + "/snp/testconnection";
-var createConnRouter = url_prefix + "/snp/createconnction";
+var createConnRouter = url_prefix + "/snp/createconnection";
